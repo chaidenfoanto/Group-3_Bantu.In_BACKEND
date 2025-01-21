dc509e67e6e44f7d2601cf4cd3fe5cf11273fc62
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2025-01-21 23:23:20 +0800

    cihuyy

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -13,6 +15,25 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(10)->create();
+        // Menambah data ke tabel users
+        DB::table('users')->insert([
+            'id_user' => 'user001',
+            'name' => 'cihuyyy',
+            'email' => 'johndoe@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password123'), // Gunakan Hash untuk enkripsi password
+            'password_confirmation' => 'password123',
+            'no_hp' => '081234567890',
+            'alamat' => 'Jl. Kebon Jeruk No. 10, Jakarta',
+            'deskripsi_alamat' => 'Dekat dengan restoran dan pusat perbelanjaan',
+            'rating' => 4.5,
+            'total_rating' => 100,
+            'foto_diri' => null, // Foto bisa ditambahkan sesuai kebutuhan
+            'remember_token' => Str::random(60),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        $userId = DB::getPdo()->lastInsertId();
     }
 }
